9cae69547a012eb5361ab0c20475b8faed9c86eb
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-05-16 23:58:54 -0600

    switched LGPL to Boost Software License

--- boost/actor/io/platform.hpp
+++ boost/actor/io/platform.hpp
@@ -1,31 +1,28 @@
 /******************************************************************************\
- *           ___        __                                                    *
- *          /\_ \    __/\ \                                                   *
- *          \//\ \  /\_\ \ \____    ___   _____   _____      __               *
- *            \ \ \ \/\ \ \ '__`\  /'___\/\ '__`\/\ '__`\  /'__`\             *
- *             \_\ \_\ \ \ \ \L\ \/\ \__/\ \ \L\ \ \ \L\ \/\ \L\.\_           *
- *             /\____\\ \_\ \_,__/\ \____\\ \ ,__/\ \ ,__/\ \__/.\_\          *
- *             \/____/ \/_/\/___/  \/____/ \ \ \/  \ \ \/  \/__/\/_/          *
- *                                          \ \_\   \ \_\                     *
- *                                           \/_/    \/_/                     *
  *                                                                            *
- * Copyright (C) 2011-2013                                                    *
+ *           ____                  _        _        _                        *
+ *          | __ )  ___   ___  ___| |_     / \   ___| |_ ___  _ __            *
+ *          |  _ \ / _ \ / _ \/ __| __|   / _ \ / __| __/ _ \| '__|           *
+ *          | |_) | (_) | (_) \__ \ |_ _ / ___ \ (__| || (_) | |              *
+ *          |____/ \___/ \___/|___/\__(_)_/   \_\___|\__\___/|_|              *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ * Copyright (C) 2011 - 2014                                                  *
  * Dominik Charousset <dominik.charousset@haw-hamburg.de>                     *
  *                                                                            *
- * This file is part of libcppa.                                              *
- * libcppa is free software: you can redistribute it and/or modify it under   *
- * the terms of the GNU Lesser General Public License as published by the     *
- * Free Software Foundation; either version 2.1 of the License,               *
- * or (at your option) any later version.                                     *
- *                                                                            *
- * libcppa is distributed in the hope that it will be useful,                 *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
- * See the GNU Lesser General Public License for more details.                *
- *                                                                            *
- * You should have received a copy of the GNU Lesser General Public License   *
- * along with libcppa. If not, see <http://www.gnu.org/licenses/>.            *
+ * Distributed under the Boost Software License, Version 1.0. See             *
+ * accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt  *
 \******************************************************************************/
+
+
+
+
+
+
+
+
+
 
 #ifndef BOOST_ACTOR_IO_PLATFORM_HPP
 #define BOOST_ACTOR_IO_PLATFORM_HPP
